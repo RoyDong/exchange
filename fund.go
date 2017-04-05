f16102f4f879a2183af949c9f60e2a844f85180d
Roy Dong <dongwei@innotechx.com>
2017-04-05 19:28:30 +0800

    .

--- fund.go
+++ fund.go
@@ -6,97 +6,17 @@ import (
 
 type Fund struct {
     //货币类型
-    currency int
+    Currency int
 
-    amount, money, fee float64
+    /*
+    期货: 多头仓位, 空头仓位
+    现货：多头仓位表示仓位，空头仓位忽略
+     */
+    LongAmount, ShortAmount float64
 
-    frozenAmount, frozenMoney float64
+    Money, Fee float64
 
-    deposit float64
-
-    bills []Bill
-
-    locker *sync.RWMutex
+    Deposit float64
 }
 
 
-func NewFund(currency int) *Fund {
-    f := &Fund{}
-    f.currency = currency
-    f.bills = make([]Bill, 0)
-    f.locker = &sync.RWMutex{}
-
-    return f
-}
-
-func (f *Fund) SetDeposit(deposit float64) {
-    f.locker.Lock()
-    defer f.locker.Unlock()
-    f.deposit = deposit
-}
-
-func (f *Fund) AddBill(bill Bill) {
-    f.locker.Lock()
-    defer f.locker.Unlock()
-
-    changed := false
-    if bill.Amount != 0 {
-        f.amount += bill.Amount
-        changed = true
-    }
-    if bill.Money != 0 {
-        f.money += bill.Money
-        changed = true
-    }
-    if bill.Fee != 0 {
-        f.fee += bill.Fee
-        changed = true
-    }
-    if changed {
-        f.bills = append(f.bills, bill)
-    }
-}
-
-func (f *Fund) ChangeFrozon(amount, money float64) {
-    f.locker.Lock()
-    defer f.locker.Unlock()
-
-    if amount != 0 {
-        f.frozenAmount += amount
-    }
-    if money != 0 {
-        f.frozenMoney += money
-    }
-}
-
-func (f *Fund) ClearBills() {
-    f.bills = make([]Bill, 0)
-}
-
-func (f *Fund) Amount() float64 {
-    f.locker.RLock()
-    defer f.locker.RUnlock()
-    return f.amount
-}
-
-func (f *Fund) Money() float64 {
-    f.locker.RLock()
-    defer f.locker.RUnlock()
-    return f.money
-}
-
-func (f *Fund) Fee() float64 {
-    f.locker.RLock()
-    defer f.locker.RUnlock()
-    return f.fee
-}
-
-func (f *Fund) Deposit() float64 {
-    f.locker.RUnlock()
-    defer f.locker.RUnlock()
-    return f.deposit
-}
-
-func (f *Fund) Currency() int {
-    return f.currency
-}
